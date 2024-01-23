d5ece4527a18c98e3ca8349eee9b90ae7f86712f
shauli aharon <shauliaharon1@gmail.com>
2024-01-23 20:25:46 +0200

    init frontend

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './DescriptionBox.css'
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+        </div>
+        <div className="descriptionbox-description">
+<p>
+An e-commerce site is an online platform that enables the buying and selling of products or services over the Internet and serves as a virtual market where businesses and individuals present their products, interact with customers and perform transactions without the need for physical presence. presence. E-commerce sites have gained immense popularity due to their convenient accessibility and the global reach they offer.
+</p>
+<p>E-commerce sites usually display products or services with detailed descriptions,
+Pictures, prices ,sizes, colors. 
+Each product usually has its own key with relevant information.</p>
+        </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
---
+++ frontend/src/Components/NewLetter/NewLetter.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './NewLetter.css'
+function NewLetter() {
+  return (
+    <div className='newletter'>
+        <h1>Get Exclusive Ofeers On Your Email</h1>
+        <p> Subscribe to our newletter and stay updated</p>
+<div>
+<input type="email" placeholder='Your Email id' />
+<button>subscribe</button>
+</div>
+    </div>
+  )
+}
+
+export default NewLetter
